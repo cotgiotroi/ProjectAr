e691da4c94183c068ca5c73ee22667b2ecd04430
Nguyen Duc Tien <cotgiotroi@gmail.com>
2022-06-06 11:16:09 +0700

    initial

---
+++ backend/src/com/tiennd/backend/BackendMain.java
@@ -0,0 +1,7 @@
+package com.tiennd.backend;
+
+public class BackendMain {
+    public static void main(String[] args) {
+        System.out.println("Backend main");
+    }
+}
---
+++ src/com/tiennd/parent/Main.java
@@ -0,0 +1,9 @@
+package com.tiennd.parent;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Test");
+    }
+}
